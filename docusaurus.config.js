4b7f67cab7d21317dd418bbddf0c8976b8c8f345
inannan423 <605031236@qq.com>
2022-07-15 11:05:19 +0800

    1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,8 +18,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'inannan423', // Usually your GitHub org/user name.
+  projectName: 'inannan423.github.io', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
