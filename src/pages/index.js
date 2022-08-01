6de9abbd95980618419dceae7a72d35a68360cee
inannan423 <605031236@qq.com>
2022-08-01 16:23:03 +0800

    11

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,40 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import { ReactNebula } from "@flodlc/nebula";
 import styles from './index.module.css';
+import Slider from "react-slick";
+//在头部引入css
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+function SimpleSlider() {
+    const settings = {
+        dots: true,//是否显示小圆点索引
+        autoplay: true,//是否自动播放
+        infinite: true,//是否无限循环
+        autoplaySpeed: 2000,//自动播放的时间
+        fade: true,//是否采用淡入淡出的效果
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+    return (
+        <div className='swiper-content'>
+            <h2> Single Item</h2>
+            <Slider {...settings} className="swiper-container">
+                <div>
+                    <h3>1</h3>
+                </div>
+                <div>
+                    <h3>2</h3>
+                </div>
+                <div>
+                    <h3>3</h3>
+                </div>
+            </Slider>
+        </div>
+    )
+}
+
+
 // 新首页
 
 function HomePageIndex() {
@@ -62,6 +96,7 @@ function HomepageHeader() {
             <img className={clsx('side', styles.side)} src="img/sidepic.png" />
           </div>
         </div>
+        {/* <SimpleSlider/> */}
       </div>
 
     </header>
