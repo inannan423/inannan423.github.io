e2f41260de0d0279bb03583f37fee49e6271d9c8
inannan423 <605031236@qq.com>
2022-08-14 15:18:10 +0800

    Update index.js

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -10,27 +10,30 @@ export default class SimpleSlider extends Component {
       slidesToShow: 1,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 1000,
+      autoplaySpeed: 3000,
       dots: false,
     };
     return (
-        <Slider {...settings}>
-          <div className={styles.picbox}>
-            <a target={"_blank"} href="https://inannan423.github.io/Svg_Board/#/">
-              <img src="https://jetzihan-img.oss-cn-beijing.aliyuncs.com/blog/svg8-6.png" />
-            </a>
-          </div>
-          <div className={styles.picbox}>
-            <a target={"_blank"} href="https://csspano.netlify.app/#/">
-              <img src="https://jetzihan-img.oss-cn-beijing.aliyuncs.com/blog/csspano86.png" />
-            </a>
-          </div>
-          <div className={styles.picbox}>
-            <a target={"_blank"} href="https://github.com/inannan423/orange-nan-mini_program">
-              <img src="https://jetzihan-img.oss-cn-beijing.aliyuncs.com/blog/wxmini86.png" />
-            </a>
-          </div>
-        </Slider>
+      <Slider {...settings}>
+        <div className={styles.picbox}>
+          <a target={"_blank"} href="https://inannan423.github.io/Svg_Board/#/">
+            <img src="https://jetzihan-img.oss-cn-beijing.aliyuncs.com/blog/svg8-6.png" />
+          </a>
+        </div>
+        <div className={styles.picbox}>
+          <a target={"_blank"} href="https://csspano.netlify.app/#/">
+            <img src="https://jetzihan-img.oss-cn-beijing.aliyuncs.com/blog/csspano86.png" />
+          </a>
+        </div>
+        <div className={styles.picbox}>
+          <a
+            target={"_blank"}
+            href="https://github.com/inannan423/orange-nan-mini_program"
+          >
+            <img src="https://jetzihan-img.oss-cn-beijing.aliyuncs.com/blog/wxmini86.png" />
+          </a>
+        </div>
+      </Slider>
     );
   }
 }
