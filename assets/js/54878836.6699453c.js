d8e978a5cc82465f5078e8262d030630258cb262
inannan423 <83146544+inannan423@users.noreply.github.com>
2022-08-20 13:58:04 +0000

    Deploying to gh-pages from  @ 81eceb8623961efcdb866697bfc7f128406b198e 🚀

--- assets/js/54878836.6699453c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkjetzihan=self.webpackChunkjetzihan||[]).push([[5758],{1750:e=>{e.exports=JSON.parse('{"permalink":"/blog/tags/\u524d\u7aef","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
--- assets/js/ee6f6aa1.a5ba38cd.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkjetzihan=self.webpackChunkjetzihan||[]).push([[1133],{2455:a=>{a.exports=JSON.parse('{"label":"\u524d\u7aef","permalink":"/blog/tags/\u524d\u7aef","allTagsPath":"/blog/tags","count":1}')}}]);
